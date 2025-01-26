b99a331bf08f2e196754d49a7dc0de11e5ef6fed
Luciano Duarte <154828881+LucianoDuarteRosa@users.noreply.github.com>
2025-01-26 08:27:36 -0300

    tentativa de responsividade

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import Sidebar from "./components/sidebar/sidebar";
 import Links from "./components/links/links";
 import Logo from "./components/logo/logo";
 import productsData from "./data.json";
-import { Grid, Container, Box } from "@mui/material";
+import { Grid, Container, Box, IconButton, Drawer } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import './index.css';
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
 
   useEffect(() => {
     // Configurar os produtos
@@ -43,11 +55,62 @@ const App = () => {
 
   return (
     <Container>
+      {/* Topo com barra de pesquisa, logo e menu hambúrguer */}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        {/* Logo - Apenas em telas grandes */}
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Logo />
+        </Box>
+
+        {/* Ícone do menu hambúrguer - Apenas em telas pequenas */}
+        <Box sx={{ display: { xs: "block", sm: "none" } }}>
+          <IconButton
+            style={{ color: "white" }}
+            onClick={toggleDrawer(true)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </IconButton>
+        </Box>
+
+        {/* Barra de pesquisa */}
+        <Box flexGrow={1} marginLeft={{ xs: 0, sm: 2 }}>
+          <SearchBar searchValue={searchValue} onSearchChange={setSearchValue} />
+        </Box>
+      </Box>
+
+      {/* Menu hambúrguer lateral */}
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        <Box className="drawer">
+          {/* Logo dentro do Drawer */}
+          <Box>
+            <Logo />
+          </Box>
+          <Sidebar
+            categories={categories}
+            selectedCategories={selectedCategories}
+            onCategoryChange={handleCategoryChange}
+          />
+          <Links />
+        </Box>
+      </Drawer>
+
+      {/* Conteúdo principal */}
       <Box marginTop={4}>
         <Grid container spacing={2}>
-          {/* Barra lateral */}
-          <Grid item xs={3}>
-            <Logo />
+          {/* Sidebar em telas grandes */}
+          <Grid
+            item
+            xs={3}
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
             <Sidebar
               categories={categories}
               selectedCategories={selectedCategories}
@@ -55,12 +118,12 @@ const App = () => {
             />
             <Links />
           </Grid>
-          {/* Pesquisa e cards de produtos */}
-          <Grid item xs={8}>
-            <SearchBar searchValue={searchValue} onSearchChange={setSearchValue} />
+
+          {/* Cards de produtos */}
+          <Grid item xs={12} sm={8}>
             <Grid container spacing={2}>
               {filteredProducts.map((product) => (
-                <Grid item xs={6} sm={4} md={3} key={product.IdProduct}>
+                <Grid item xs={6} sm={6} md={4} key={product.IdProduct}>
                   <ProductCard product={product} />
                 </Grid>
               ))}
@@ -70,6 +133,6 @@ const App = () => {
       </Box>
     </Container>
   );
-};
+}
 
 export default App;
